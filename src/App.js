8001677d8fdf8b7d48fb4cef9f81212dc24da647
Carlos Martinez <curlosmart@gmail.com>
2022-02-27 15:17:18 -0500

    Setup files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+
+const App = () => {
+
+
+  return (
+    <div className="main">
+      Advice #117
+    </div>
+  );
+}
+
+export default App;
