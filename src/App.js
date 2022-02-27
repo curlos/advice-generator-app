da9737a2d6848ada80e6b1af0f127c3d489594a7
Carlos Martinez <curlosmart@gmail.com>
2022-02-27 15:54:33 -0500

    fetch random advice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
 
+  const [advice, setAdvice] = useState('')
+
+  useEffect(() => {
+    fetchRandomAdvice()
+  }, [])
+
+  const fetchRandomAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice').then((response) => {
+      console.log(response.data.slip)
+      setAdvice(response.data.slip)
+    })
+  }
+
 
   return (
     <div className="main">
-      Advice #117
+      <div className="middleContainer">
+        <div className="header">
+          ADVICE #{advice.id}
+        </div>
+
+        <div className="adviceText">"{advice.advice}"</div>
+
+        <div className="pauseContainer">
+          <i class="fas fa-pause"></i>
+        </div>
+
+        <button className="randomButton" onClick={fetchRandomAdvice}>
+          <i class="fas fa-dice-five"></i>
+        </button>
+      </div>
     </div>
   );
 }
